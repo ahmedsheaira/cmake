2a59deba642034aa21afdf0d761331a5c8f4260d
ahmedsheaira <sheaira.ahmed@gmail.com>
2024-04-28 18:28:53 +0300

    Add wifi library with its CMake file

---
+++ app_1/lib/wifi.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "wifi.hpp"
+
+void wifi_init() {
+    std::cout << "WIFI Init is working ..." << std::endl;
+}
---
+++ app_1/lib/wifi.hpp
@@ -0,0 +1,6 @@
+#ifndef WIFI_HPP_
+#define WIFI_HPP_
+
+void wifi_init();
+
+#endif
