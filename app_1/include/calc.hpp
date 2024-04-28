682f9793decc846ed1e2feccd33f34939af59789
ahmedsheaira <sheaira.ahmed@gmail.com>
2024-04-28 03:43:36 +0300

    Application version one

---
+++ app_1/include/calc.hpp
@@ -0,0 +1,7 @@
+#ifndef CALC_HPP_
+#define CALC_HPP_
+
+#define X 10
+int sum(int x, int y);
+
+#endif
---
+++ app_1/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "calc.hpp"
+
+int main() {
+    std::cout << X << std::endl;
+    // std:: cout << sum(1, 2) << std::endl;
+    return 0;
+}
